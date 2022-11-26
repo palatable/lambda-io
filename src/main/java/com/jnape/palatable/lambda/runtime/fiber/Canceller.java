29dee44db213e660bc1dec40737d6964c59c8518
jnape <jnape09@gmail.com>
2022-11-25 19:30:47 -0600

    Canceller

---
+++ src/main/java/com/jnape/palatable/lambda/runtime/fiber/Canceller.java
@@ -0,0 +1,85 @@
+package com.jnape.palatable.lambda.runtime.fiber;
+
+import com.jnape.palatable.shoki.impl.HashSet;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.jnape.palatable.lambda.runtime.fiber.CancelState.fresh;
+import static com.jnape.palatable.shoki.impl.HashSet.hashSet;
+
+public sealed interface Canceller permits ThreadSafeCancelTree {
+
+    void cancel();
+
+    boolean cancelled();
+
+    Canceller addChild();
+
+    void removeChild(Canceller child);
+
+    static Canceller root() {
+        return new ThreadSafeCancelTree(null);
+    }
+}
+
+final class ThreadSafeCancelTree implements Canceller {
+
+    private final Canceller                    parent;
+    private final AtomicReference<CancelState> stateRef = new AtomicReference<>(fresh());
+
+    ThreadSafeCancelTree(Canceller parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void cancel() {
+        CancelState cancelState = stateRef.getAndSet(CancelState.cancelled());
+        if (!cancelState.isCancelled()) {
+            cancelState.children().forEach(Canceller::cancel);
+            if (parent != null)
+                parent.removeChild(this);
+        }
+    }
+
+    @Override
+    public boolean cancelled() {
+        return stateRef.get().isCancelled();
+    }
+
+    @Override
+    public Canceller addChild() {
+        ThreadSafeCancelTree child = new ThreadSafeCancelTree(this);
+        return stateRef
+                       .updateAndGet(cs -> cs.addChild(child))
+                       .isCancelled()
+               ? this
+               : child;
+    }
+
+    @Override
+    public void removeChild(Canceller child) {
+        stateRef.updateAndGet(cs -> cs.removeChild(child));
+    }
+}
+
+record CancelState(boolean isCancelled, HashSet<Canceller> children) {
+
+    private static final CancelState CANCELLED = new CancelState(true, hashSet());
+    private static final CancelState FRESH     = new CancelState(false, hashSet());
+
+    CancelState addChild(Canceller child) {
+        return isCancelled ? this : new CancelState(false, children.add(child));
+    }
+
+    CancelState removeChild(Canceller child) {
+        return new CancelState(isCancelled, children.remove(child));
+    }
+
+    public static CancelState fresh() {
+        return FRESH;
+    }
+
+    public static CancelState cancelled() {
+        return CANCELLED;
+    }
+}
---
+++ src/test/java/com/jnape/palatable/lambda/runtime/fiber/CancellerTest.java
@@ -0,0 +1,84 @@
+package com.jnape.palatable.lambda.runtime.fiber;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CancellerTest {
+
+    private Canceller canceller;
+
+    @BeforeEach
+    public void setUp() {
+        canceller = Canceller.root();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        canceller.cancel();
+        canceller = null;
+    }
+
+    @Test
+    public void isNotCancelledByDefault() {
+        assertFalse(canceller.cancelled());
+    }
+
+    @Test
+    public void IsCancelledAfterCallingCancel() {
+        canceller.cancel();
+        assertTrue(canceller.cancelled());
+    }
+
+    @Test
+    public void descendantsAreCancelledAndRemoved() {
+        Canceller child1      = canceller.addChild();
+        Canceller child2      = canceller.addChild();
+        Canceller grandChild1 = child1.addChild();
+        Canceller grandChild2 = child2.addChild();
+
+        assertNotSame(canceller, child1);
+        assertNotSame(canceller, child2);
+        assertNotSame(child1, grandChild1);
+        assertNotSame(child2, grandChild2);
+
+        canceller.cancel();
+        assertTrue(canceller.cancelled());
+        assertTrue(child1.cancelled());
+        assertTrue(child2.cancelled());
+        assertTrue(grandChild1.cancelled());
+        assertTrue(grandChild2.cancelled());
+    }
+
+    @Test
+    public void childrenAreNotAddedWhenCancelled() {
+        canceller.cancel();
+        Canceller child = canceller.addChild();
+        assertSame(canceller, child);
+        assertTrue(child.cancelled());
+    }
+
+    @Test
+    public void removeChildPreventsCancellationOfChild() {
+        Canceller child1 = canceller.addChild();
+        Canceller child2 = canceller.addChild();
+
+        assertNotSame(canceller, child1);
+        assertNotSame(canceller, child2);
+
+        canceller.removeChild(child2);
+        canceller.cancel();
+
+        assertTrue(canceller.cancelled());
+        assertTrue(child1.cancelled());
+        assertFalse(child2.cancelled());
+
+        child2.cancel();
+        assertTrue(child2.cancelled());
+    }
+}
