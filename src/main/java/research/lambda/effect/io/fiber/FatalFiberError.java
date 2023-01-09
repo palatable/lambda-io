f1c8b1f866666ec0eeae42f5a81fa093bc6b69f8
jnape <jnape09@gmail.com>
2023-01-08 18:58:31 -0600

    Removing old research fiber

--- src/main/java/research/lambda/effect/io/fiber/FatalFiberError.java
+++
@@ -1,7 +0,0 @@
-package research.lambda.effect.io.fiber;
-
-abstract class FatalFiberError extends Error {
-    FatalFiberError(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
