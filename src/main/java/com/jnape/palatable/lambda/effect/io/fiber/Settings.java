67981ab14dec13939d2eff98899c27dca37613ed
jnape <jnape09@gmail.com>
2023-02-20 22:13:10 -0600

    settings cleanup

--- src/main/java/com/jnape/palatable/lambda/effect/io/fiber/Settings.java
+++ src/main/java/com/jnape/palatable/lambda/effect/io/fiber/Settings.java
@@ -2,13 +2,12 @@ package com.jnape.palatable.lambda.effect.io.fiber;
 
 import com.jnape.palatable.lambda.adt.Maybe;
 import com.jnape.palatable.lambda.functions.Fn1;
-import com.jnape.palatable.lambda.functions.specialized.Kleisli;
 import com.jnape.palatable.shoki.impl.HashMap;
 
 import static com.jnape.palatable.lambda.adt.Maybe.maybe;
 import static com.jnape.palatable.lambda.adt.Try.trying;
 import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
-import static com.jnape.palatable.lambda.monoid.builtin.First.first;
+import static com.jnape.palatable.lambda.functions.Fn1.fn1;
 import static com.jnape.palatable.shoki.impl.HashMap.hashMap;
 import static java.lang.Integer.parseInt;
 import static java.lang.System.getProperty;
@@ -18,24 +17,19 @@ public final class Settings {
 
     private static final HashMap<String, Boolean> booleanMap = hashMap(tuple("true", true), tuple("false", false));
 
-    private static final Kleisli<String, String, Maybe<?>, Maybe<String>> readProperty = s -> maybe(getProperty(s));
-    private static final Kleisli<String, String, Maybe<?>, Maybe<String>> readEnv      = s -> maybe(getenv(s));
-
-    private static final Kleisli<String, Boolean, Maybe<?>, Maybe<Boolean>> parseBool        = s -> booleanMap.get(s.toLowerCase());
-    private static final Fn1<String, Maybe<Boolean>>                        readBoolProperty = readProperty.andThen(parseBool);
-    private static final Fn1<String, Maybe<Boolean>>                        readBoolEnv      = readEnv.andThen(parseBool);
-    private static final Kleisli<String, Integer, Maybe<?>, Maybe<Integer>> parseInt         = s -> trying(() -> parseInt(s)).toMaybe();
-    private static final Fn1<String, Maybe<Integer>>                        readIntProperty  = readProperty.andThen(parseInt);
-    private static final Fn1<String, Maybe<Integer>>                        readIntEnv       = readEnv.andThen(parseInt);
-
     private Settings() {
     }
 
     public static Maybe<Boolean> loadBoolean(String label) {
-        return first(readBoolProperty.apply(label), readBoolEnv.apply(label));
+        return loadAndParse(label, fn1(booleanMap::get).contraMap(String::toLowerCase));
     }
 
     public static Maybe<Integer> loadInteger(String label) {
-        return first(readIntProperty.apply(label), readIntEnv.apply(label));
+        return loadAndParse(label, v -> trying(() -> parseInt(v)).toMaybe());
+    }
+
+    private static <A> Maybe<A> loadAndParse(String label, Fn1<? super String, ? extends Maybe<A>> tryParse) {
+        return maybe(getProperty(label)).flatMap(tryParse).fmap(Maybe::just)
+                .orElseGet(() -> maybe(getenv(label)).flatMap(tryParse));
     }
 }
